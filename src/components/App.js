e2f810ffa6bd028b841206d733f7e30d7db4b428
souravgreatest <jainsourav220@gmail.com>
2025-04-29 22:19:42 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+      {/* <UserList/> */}
+      <Routes>
+        <Route path="/" element={<UserList/>}/>
+        <Route path="/users/:id" element={<UserDetails/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
